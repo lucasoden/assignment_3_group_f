f61b948e8e5d731d3a68b823120ca2777f240458
lucasoden <113351500+lucasoden@users.noreply.github.com>
2025-10-13 12:02:30 +0200

    Revert "ffa"
    This reverts commit a6ddf768ab7018a4ffbc67ff5a494a303186fe4c.

--- .github/workflows/dataset.py
+++ .github/workflows/dataset.py
@@ -1,10 +1,8 @@
 from sklearn.datasets import load_diabetes
 
-def test_load_and_print():
-    Xy = load_diabetes(as_frame=True)
-    X = Xy.frame.drop(columns=["target"])
-    y = Xy.frame["target"]
+Xy = load_diabetes(as_frame=True)
+X = Xy.frame.drop(columns=["target"])
+y = Xy.frame["target"]  # acts as a "progression index" (higher = worse)
 
-    print(X)
-    print(y)
-    assert len(X) > 0  # just a simple assertion for the test to pass
+print(X)
+print(y)
