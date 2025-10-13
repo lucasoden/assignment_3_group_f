c6d37f8eee447184005a31cdf124eea48da34b29
SiriMSel <siri@selleby.com>
2025-10-13 17:12:54 +0200

    Error fix changed name of parameter

--- ml/metrics.py
+++ ml/metrics.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error
 
 
 def rmse(y_true, y_pred) -> float:
-    return mean_squared_error(y_true, y_pred, squared=False)
+    return root_mean_squared_error(y_true, y_pred)
