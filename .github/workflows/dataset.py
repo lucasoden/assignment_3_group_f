a6ddf768ab7018a4ffbc67ff5a494a303186fe4c
lucasoden <113351500+lucasoden@users.noreply.github.com>
2025-10-13 11:59:19 +0200

    ffa

--- .github/workflows/dataset.py
+++ .github/workflows/dataset.py
@@ -1,8 +1,10 @@
 from sklearn.datasets import load_diabetes
 
-Xy = load_diabetes(as_frame=True)
-X = Xy.frame.drop(columns=["target"])
-y = Xy.frame["target"]  # acts as a "progression index" (higher = worse)
+def test_load_and_print():
+    Xy = load_diabetes(as_frame=True)
+    X = Xy.frame.drop(columns=["target"])
+    y = Xy.frame["target"]
 
-print(X)
-print(y)
+    print(X)
+    print(y)
+    assert len(X) > 0  # just a simple assertion for the test to pass
